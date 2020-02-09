a5ecf819e98895bdebaa6c43b24469bc9c69feb1
Irena Jurasek <irenaskydive@gmail.com>
2020-02-09 12:00:49 +0100

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -43,11 +43,11 @@ const printCells = (state) => {
   return accumulator;
 };
 
-const getNeighborsOf = ([x, y]) => {
+const getNeighborsOf = ([x, y]) => [
   [x-1, y+1], [x, y+1], [x+1, y+1],
   [x-1, y],             [x+1, y],
   [x-1, y-1], [x, y-1], [x+1, y-1]
-};
+]; 
 
 const getLivingNeighbors = (cell, state) => {
   return getLivingNeighbors(cell).filter((n) => contains.bind(state)(n));
